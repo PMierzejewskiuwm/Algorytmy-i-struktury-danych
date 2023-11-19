3b6a518c018fbc00bf144bb5badafcdfa40a1fa9
patryk.mierzejewski <115549303+PMierzejewskiuwm@users.noreply.github.com>
2023-11-19 17:15:57 +0100

    Add files via upload

---
+++ insertSort/insertSort/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace insertSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(296, 224);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(164, 79);
+            this.btnSort.TabIndex = 0;
+            this.btnSort.Text = "Insert Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSort;
+    }
+}
+
---
+++ insertSort/insertSort/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace insertSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            int[] tab = { 1, 6, 9, 2, 8, 12 };
+            insertSort(tab);
+            MessageBox.Show(toString(tab));
+        }
+
+        string toString(int[] tab)
+        {
+            string result = "";
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i].ToString();
+                result += tab[i];
+                result += " ";
+            }
+            return result;
+        }
+        void insertSort(int[] tab)
+        {
+            for(int i = 1; i < tab.Length; i++)
+            {
+                int temp = tab[i];
+                int j = i - 1;
+                while(j >= 0 && tab[j] > temp)
+                {
+                    tab[j + 1] = tab[j];
+                    j--;
+                }
+                tab[j + 1] = temp;
+            }
+        }
+    }
+}
