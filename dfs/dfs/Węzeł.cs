a1bcfa1650f527532cb1cd6862a5474a5c942243
patryk.mierzejewski <115549303+PMierzejewskiuwm@users.noreply.github.com>
2023-11-26 22:03:42 +0100

    Add files via upload

---
+++ dfs/dfs/Węzeł.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dfs
+{
+    internal class Węzeł
+    {
+        public int wartosc;
+        public List<Węzeł> dzieci = new List<Węzeł>();
+        public Węzeł(int liczba)
+        {
+            this.wartosc = liczba;
+        }
+    }
+}
