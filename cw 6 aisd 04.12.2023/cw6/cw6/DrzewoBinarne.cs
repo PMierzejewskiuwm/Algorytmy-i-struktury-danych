6690ee0cd1627d70611b9bdab1f2544fcbf0b6f0
patryk.mierzejewski <115549303+PMierzejewskiuwm@users.noreply.github.com>
2023-12-10 17:22:28 +0100

    Add files via upload

---
+++ cw 6 aisd 04.12.2023/cw6/cw6/DrzewoBinarne.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw6
+{
+    internal class DrzewoBinarne
+    {
+        public Węzeł3 korzeń;
+        public DrzewoBinarne(int liczba)
+        {
+            this.korzeń = new Węzeł3(liczba);
+        }
+        void Add(int liczba)
+        {
+            Węzeł3 rodzic = this.ZnajdzRodzica(liczba);
+            rodzic.Add(liczba);
+        }
+
+        private Węzeł3 ZnajdzRodzica(int liczba)
+        {
+            Węzeł3 obecny = korzeń;
+            Węzeł3 rodzic = null;
+
+            while (obecny != null)
+            {
+                rodzic = obecny;
+                if (liczba < obecny.wartosc)
+                {
+                    obecny = obecny.leweDziecko;
+                }
+                else
+                {
+                    obecny = obecny.praweDziecko;
+                }
+            }
+
+            return rodzic;
+        }
+        //BST
+    }
+}
---
+++ cw 6 aisd 04.12.2023/cw6/cw6/Węzeł3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw6
+{
+    internal class Węzeł3
+    {
+        public int wartosc;
+        public Węzeł3 rodzic;
+        public Węzeł3 leweDziecko;
+        public Węzeł3 praweDziecko;
+        
+        public Węzeł3(int liczba)
+        {
+            this.wartosc = liczba;
+        }
+        public override string ToString()
+        {
+            return this.wartosc.ToString();
+        }
+
+        internal void Add(int liczba)
+        {
+            if (liczba < this.wartosc)
+            {
+                if (leweDziecko == null)
+                {
+                    leweDziecko = new Węzeł3(liczba);
+                    leweDziecko.rodzic = this;
+                }
+                else
+                {
+                    leweDziecko.Add(liczba);
+                }
+            }
+            else
+            {
+                if (praweDziecko == null)
+                {
+                    praweDziecko = new Węzeł3(liczba);
+                    praweDziecko.rodzic = this;
+                }
+                else
+                {
+                    praweDziecko.Add(liczba);
+                }
+            }
+        }
+    }
+}
