b150c3e2ea65d751b49bc4c59c407f588f88e71e
patryk.mierzejewski <115549303+PMierzejewskiuwm@users.noreply.github.com>
2023-12-19 09:15:06 +0100

    Add files via upload

---
+++ cw 19.12.2023/cw6/cw6/Krawędź.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw6
+{
+    internal class Krawędź
+    {
+        public Krawędź waga;
+        public Krawędź początek;
+        public Krawędź koniec;
+    }
+}
---
+++ cw 19.12.2023/cw6/cw6/Wezeł.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw6
+{
+    internal class Wezeł
+    {
+        public Wezeł wartość;
+        public List<Wezeł> krawędzie = new List<Wezeł>();
+    }
+}
